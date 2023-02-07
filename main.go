817b514a12d6af28ec0a86bc19d413484334e249
Kris Leungwattanakij <kleungwa@purdue.edu>
2023-02-07 18:25:10 -0500

    Implemented change info function

--- main.go
+++ main.go
@@ -52,5 +52,34 @@ func main() {
 		return c.JSON(hwList)
 	})
 
+	app.Patch("/api/changeinfo/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		newHW := Homework{}
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+		if err := c.BodyParser(&newHW); err != nil {
+			return err
+		}
+
+		for i, hw := range hwList {
+			if hw.ID == id {
+				if hw.Title != newHW.Title {
+					hwList[i].Title = newHW.Title
+				}
+				if hw.Body != newHW.Body {
+					hwList[i].Body = newHW.Body
+				}
+				if hw.DueDate != newHW.DueDate {
+					hwList[i].DueDate = newHW.DueDate
+				}
+				break
+			}
+		}
+
+		return c.JSON(hwList)
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
