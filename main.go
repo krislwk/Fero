5aa530de3ac9362bac527ef32ddb0b2d3880852b
Kris Leungwattanakij <kleungwa@purdue.edu>
2023-02-07 17:45:45 -0500

    Revert "finished homework creation and updating APIs"
    This reverts commit 73fc2c6f0209e1f05b68e5b5fe197e5fa6637776.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"log"
 )
@@ -8,12 +9,11 @@ import (
 type Homework struct {
 	ID       int    `json:"id"`
 	taskName string `json:"name"`
-	body     string `json:"body"`
-	dueDate  string `json:"duedate"` //"DD/MM/YYYY"
-	status   bool   `json:"status"`
+	Status   bool   `json:"status"`
 }
 
 func main() {
+	fmt.Println("Hello World!")
 	app := fiber.New()
 	hwList := []Homework{}
 
@@ -23,37 +23,9 @@ func main() {
 
 	app.Post("/api/homeworks", func(c *fiber.Ctx) error {
 		hw := &Homework{}
-		err := c.BodyParser(&hw)
-
-		if err != nil {
-			return err
-		}
-
 		hw.ID = len(hwList)
-		hw.status = false
+		hw.Status = false
 		hwList = append(hwList, *hw)
-		return c.JSON(hwList)
-	})
-
-	app.Get("/getHomeworks", func(c *fiber.Ctx) error {
-		return c.JSON(hwList)
-	})
-
-	app.Patch("/api/homeworks/:id/:done", func(c *fiber.Ctx) error {
-		id, idErr := c.ParamsInt("id")
-		done := c.Params("done")
-
-		if idErr != nil {
-			return c.Status(401).SendString("Invalid ID")
-		}
-
-		for i, t := range hwList {
-			if t.ID == id {
-				hwList[i].status = (done == "done")
-				break
-			}
-		}
-
 		return c.JSON(hwList)
 	})
 
